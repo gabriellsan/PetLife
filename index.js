5e3327ff21b6d333f9cb9752fcd4ee7f05ec9f5e
Gabriel Santana Santos <gabrielsantana123santos@gmail.com>
2023-10-22 02:37:58 -0300

    menu hamburguer

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ elementosDuvida.forEach(
         })
     }
 )
+
+function menuShow() {
+  let menuMobile = document.querySelector('.mobile-menu');
+  if (menuMobile.classList.contains('open')) {
+      menuMobile.classList.remove('open');
+      document.querySelector('.icon').src = "./src/assets/menu_white_36dp.svg";
+  } else {
+      menuMobile.classList.add('open');
+      document.querySelector('.icon').src = "./src/assets/close_white_36dp.svg";
+  }
+}
